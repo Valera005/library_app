b26e703e415d595ef8729ba66202151e1abbf210
Valerii Kaspruk <kaspruk05@gmail.com>
2024-07-08 11:22:00 +0100

    initial react app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">Book library app</header>
+    </div>
+  );
+}
+
+
+export default App;
